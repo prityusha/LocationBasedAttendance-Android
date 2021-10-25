d303a4e1bb364f04d431fd5aa61ef85b5444defb
majestic-rahul <79097971+majestic-rahul@users.noreply.github.com>
2021-10-25 10:12:46 +0530

    Added Ui to app,connected login to firebase,added logo (#10)

--- app/src/main/java/com/example/android/geo_loco/ReadWriteUserDetail.java
+++ app/src/main/java/com/example/android/geo_loco/ReadWriteUserDetail.java
@@ -3,12 +3,13 @@ package com.example.android.geo_loco;
 import java.io.Serializable;
 
 public class ReadWriteUserDetail  implements Serializable {
-    public String textFullName ,textDob , textGender ,textEnrollment;
+    public String textFullName ,textDob , textGender ,textEnrollment ,textBatch;
 
-    public ReadWriteUserDetail(String textFullName , String textEnrollment , String textDob ,String textGender){ ;
+    public ReadWriteUserDetail(String textFullName , String textEnrollment , String textDob ,String textGender , String textBatch){ ;
         this.textFullName = textFullName;
         this.textEnrollment = textEnrollment;
         this.textDob = textDob;
         this.textGender = textGender;
+        this.textBatch = textBatch;
     }
 }
