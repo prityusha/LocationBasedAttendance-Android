4eec927259ebc993834ca2b92863d91b8abb3cbf
Prityusha Priyadarshi <54525593+prityusha@users.noreply.github.com>
2021-11-26 23:20:33 +0530

    Made more fences and added visibility to mark attendance button (#26)

--- app/src/main/java/com/example/android/geo_loco/GoefenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/geo_loco/GoefenceBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.Geofence;
@@ -19,21 +20,23 @@ public class GoefenceBroadcastReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         //Toast.makeText(context, "Geofence Triggered........", Toast.LENGTH_LONG).show();
+        Log.d("first","Geofence Triggered........");
 
         NotificationHelper notificationHelper = new NotificationHelper(context);
 
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
 
-        Bundle extras=intent.getExtras();
+        /*Bundle extras=intent.getExtras();
         if(extras!=null){
             ClassRoomNumber=extras.getString("key");
-        }
+            Log.d("first",ClassRoomNumber);
+        }*/
         if(geofencingEvent.hasError()){
             Log.d("AskPermission", "onReceive: Error Receiving Geofence Event..");
             return;
         }
 
-        List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
+
         int transitionType=geofencingEvent.getGeofenceTransition();
 
         switch (transitionType){
@@ -41,8 +44,13 @@ public class GoefenceBroadcastReceiver extends BroadcastReceiver {
                 Toast.makeText(context, "ENTERED INTO GEOFENCE", Toast.LENGTH_SHORT).show();
                 break;
             case Geofence.GEOFENCE_TRANSITION_DWELL:
-                Toast.makeText(context, "DWELLING INTO GEOFENCE", Toast.LENGTH_SHORT).show();
-                notificationHelper.sendHighPriorityNotification("Entered "+ClassRoomNumber,"",MainActivity.class);
+                //Toast.makeText(context, "DWELLING INTO GEOFENCE", Toast.LENGTH_SHORT).show();
+                List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
+                for(Geofence geofence: geofences){
+                    notificationHelper.sendHighPriorityNotification("Entered "+geofence.getRequestId()+", Mark Your Attendance","",MainActivity.class);
+                    MainActivity.updateButton.setVisibility(View.VISIBLE);
+                    Log.d("first",geofence.getRequestId());
+                }
                 break;
             case Geofence.GEOFENCE_TRANSITION_EXIT:
                 Toast.makeText(context, "EXITING FROM GEOFENCE", Toast.LENGTH_SHORT).show();
